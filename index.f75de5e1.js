ac01cdd33594484f14e1d5e1f3df33feaf07ca0f
Vadym Bryvka <vadym.bryvkaa@gmail.com>
2024-06-09 21:25:47 -0500

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,24 @@
+"use strict";
+const populationElements = [
+    ...document.querySelectorAll(".population")
+];
+function calculateTotals(elements) {
+    let sum = 0;
+    const count = elements.length;
+    elements.forEach((element)=>{
+        const population = parseInt(element.textContent.replace(/,/g, ""), 10);
+        sum += population;
+    });
+    const average = sum / count;
+    return {
+        sum,
+        average
+    };
+}
+const totals = calculateTotals(populationElements);
+const totalPopulation = totals.sum;
+const averagePopulation = totals.average;
+document.querySelector(".total-population").textContent = totalPopulation.toLocaleString("en-US");
+document.querySelector(".average-population").textContent = averagePopulation.toLocaleString("en-US");
+
+//# sourceMappingURL=index.f75de5e1.js.map
